eb678d9462dddee140d9992cf40e19cbd3a5d092
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:48:16 +0200

    Add files via upload

---
+++ PB11EXAM/PB11EXAM/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PB11EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //11.Afisati in ordine inversa cifrele unui numar n. 
+
+            int n, oglindit = 0;
+
+            n = int.Parse(Console.ReadLine());
+
+            while (n!=0)
+            {
+                oglindit = oglindit * 10 + n % 10;
+                n = n / 10;
+            }
+            Console.WriteLine(oglindit);
+        }
+    }
+}
