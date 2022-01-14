656708466438483249507ab78d3fd51eb3926b8d
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:43:57 +0200

    Add files via upload

---
+++ PB3EXAM/PB3EXAM/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PB3EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3.Determinati daca n se divide cu k, unde n si k sunt date de intrare. 
+
+            int n, k;
+
+            n = int.Parse(Console.ReadLine());
+
+            k = int.Parse(Console.ReadLine());
+
+            if (n % k == 0) Console.WriteLine("n este divizibil cu k");
+            else Console.WriteLine("n nu este divizibil cu k");
+        }
+    }
+}
