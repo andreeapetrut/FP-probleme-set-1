906fb1e974aa9aab22e3de830da7311dcd9c1555
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:45:36 +0200

    Add files via upload

---
+++ PB6EXAM/PB6EXAM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PB6EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //6.Detreminati daca trei numere pozitive a, b si c pot fi lungimile laturilor unui triunghi. 
+
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+
+            b = int.Parse(Console.ReadLine());
+
+            c = int.Parse(Console.ReadLine());
+
+            if ((a + b > c) && (a + c > b) && (b + c > a))
+            {
+                Console.WriteLine("Se poate forma un triunghi");
+            }
+            else Console.WriteLine("Nu se poate forma un triunghi");
+        }
+    }
+}
