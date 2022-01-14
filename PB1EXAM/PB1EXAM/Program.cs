e9f36322fb532a3097ac9ba5d194d859b86a7c38
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:42:17 +0200

    Add files via upload

---
+++ PB1EXAM/PB1EXAM/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PB1EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Rezolvati ecuatia de gradul 1 cu o necunoscuta: ax+b = 0, unde a si b sunt date de intrare. 
+
+            float a, b;
+
+            a = float.Parse(Console.ReadLine());
+
+            b = float.Parse(Console.ReadLine());
+
+            if (a == 0)
+            {
+
+                Console.WriteLine("Nu exista solutie");
+            }
+            else Console.WriteLine(-b / a);
+        }
+    }
+}
