37e7eab8b006532d94fa2d33d1fcaf6e92ac9700
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:46:38 +0200

    Add files via upload

---
+++ PB8EXAM/PB8EXAM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PB8EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //8.(Swap restrictionat) Se dau doua variabile numerice a si b ale carori valori sunt date de intrare.
+            //Se cere sa se inverseze valorile lor fara a folosi alte variabile suplimentare.  
+            int a, b;
+
+            a = int.Parse(Console.ReadLine());
+
+            b = int.Parse(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("a=" + a);
+
+            Console.WriteLine("b=" + b);
+        }
+    }
+}
