2b159538a7196966192e39a3005211f69c520b35
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:50:18 +0200

    Add files via upload

---
+++ PB15EXAM/PB15EXAM/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PB15EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //15.Se dau 3 numere. Sa se afiseze in ordine crescatoare. 
+
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+
+            b = int.Parse(Console.ReadLine());
+
+            c = int.Parse(Console.ReadLine());
+
+            if (a > b && b > c) Console.WriteLine(c + " " + b + " " + a);
+            else if (a > c && c > b) Console.WriteLine(b + " " + c + " " + a);
+            else if (b > a && a > c) Console.WriteLine(c + " " + a + " " + b);
+            else if (b > c && c > a) Console.WriteLine(a + " " + c + " " + b);
+            else if (c > a && a > b) Console.WriteLine(b + " " + a + " " + c);
+            else if (c > b && b > a) Console.WriteLine(a + " " + b + " " + c);
+        }
+    }
+}
