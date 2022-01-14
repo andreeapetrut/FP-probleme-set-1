8649fc1e6aed92f0a81c9a8502fb5abfc8282c60
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:45:05 +0200

    Add files via upload

---
+++ PB5EXAM/PB5EXAM/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PB5EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5.Extrageti si afisati a k-a cifra de la sfarsitul unui numar. Cifrele se numara de la dreapta la stanga. 
+
+            int nr = 0, n, k;
+
+            n = int.Parse(Console.ReadLine());
+
+            k = int.Parse(Console.ReadLine());
+
+            if (n == 0)
+            {
+
+                Console.WriteLine(n);
+            }
+            else
+                while (n != 0)
+                {
+                    if (nr == k)
+                        Console.WriteLine(n % 10);
+                    nr++;
+                    n = n / 10;
+                }
+        }
+    }
+}
