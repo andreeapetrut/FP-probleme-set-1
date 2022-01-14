eb50af8ea4aee8df9a0dfb8aeac716dc52c39f9c
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:49:46 +0200

    Add files via upload

---
+++ PB14EXAM/PB14EXAM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PB14EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //14.Determianti daca un numar n este palindrom. 
+            //(un numar este palindrom daca citit invers obtinem un numar egal cu el, de ex. 121 sau 12321.
+
+            int n, palindrom = 0, x;
+
+            n = int.Parse(Console.ReadLine());
+
+            x = n;
+            while (n!=0)
+            {
+                palindrom = palindrom * 10 + n % 10;
+                n = n / 10;
+            }
+            if (palindrom == x) Console.WriteLine("Numarul este palindrom");
+            else Console.WriteLine("Numarul nu este palindrom");
+        }
+    }
+}
