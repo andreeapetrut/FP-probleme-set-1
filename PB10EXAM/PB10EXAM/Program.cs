fca0367f1fa8be7453d3e9dfa4fd4b61d9ab9ac1
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:47:45 +0200

    Add files via upload

---
+++ PB10EXAM/PB10EXAM/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PB10EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //10.Test de primalitate: determinati daca un numar n este prim.
+
+            int n;
+
+            bool prim = true;
+
+            n = int.Parse(Console.ReadLine());
+
+            if ((n == 1) || (n == 0))
+            {
+
+                Console.WriteLine("Numarul nu este prim");
+            }
+            else
+            {
+                for (int i = 2; i <= n / 2; i++)
+                {
+                    if (n % i == 0) prim = false;
+                }
+                if (prim == true) Console.WriteLine("Numarul este prim");
+                else Console.WriteLine("Numarul nu este prim");
+            }
+        }
+    }
+}
