2d8f34700002c7d1dfb05046ff6d571190240e7a
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:49:19 +0200

    Add files via upload

---
+++ PB13EXAM/PB13EXAM/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PB13EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //13.Determianti cati ani bisecti sunt intre anii y1 si y2.
+            int y1, y2, nr = 0;
+
+            bool bisect;
+
+            y1 = int.Parse(Console.ReadLine());
+            y2 = int.Parse(Console.ReadLine());
+
+            for (int i=y1;i<=y2;i++)
+            {
+                bisect = false;
+                if ((i % 4 == 0) && (i % 100 != 0)) bisect = true;
+                if (i % 400==0) bisect = true;
+                if (bisect == true) nr++;
+            }
+            Console.WriteLine("Sunt " + nr + " ani bisecti");
+        }
+    }
+}
