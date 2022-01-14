5714a6105d49c956195225af47b2a4bf7a495f67
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:47:07 +0200

    Add files via upload

---
+++ PB9EXAM/PB9EXAM/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PB9EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //9.Afisati toti divizorii numarului n. 
+
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Divizorii lui " + n + " sunt: ");
+
+            for (int i=1;i<=n;i++)
+            {
+                if (n % i == 0) Console.Write(i + " ");
+            }
+        }
+    }
+}
