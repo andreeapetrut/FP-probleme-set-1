c70af7fb6df30d9031ab43739946b226a9566b69
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:50:58 +0200

    Add files via upload

---
+++ PB16EXAM/PB16EXAM/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PB16EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //16.Se dau 5 numere. Sa se afiseze in ordine crescatoare.
+
+            int[] v = new int[5];
+
+            for (int i=0;i<5;i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            //SORTARE
+            for (int i=0;i<4;i++)
+            {
+                for (int j=i+1;j<5;j++)
+                {
+                    if (v[i]>v[j])
+                    {
+                        int aux;
+                        aux = v[i];
+                        v[i] = v[j];
+                        v[j] = aux;
+                    }
+                }
+            }
+            //Afisarea celor 5 numere dupa sortare.
+            for (int i=0;i<5;i++)
+            {
+                Console.Write(v[i] + " ");
+            }
+        }
+    }
+}
