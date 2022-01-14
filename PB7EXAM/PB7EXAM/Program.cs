f2e8f735ea5bf1baf95478b8f8212a4bf8439ae8
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:46:08 +0200

    Add files via upload

---
+++ PB7EXAM/PB7EXAM/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PB7EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //7.(Swap) Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor. 
+            int a, b, aux;
+
+            a = int.Parse(Console.ReadLine());
+
+            b = int.Parse(Console.ReadLine());
+         
+            aux = a;
+            a = b;
+            b = aux;
+
+            Console.WriteLine("a=" + a);
+
+            Console.WriteLine("b=" + b);
+        }
+    }
+}
