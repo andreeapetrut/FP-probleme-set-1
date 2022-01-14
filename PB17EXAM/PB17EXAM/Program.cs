16cd55614568dd37f8f7b0a6cc69dfbfff998791
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:51:34 +0200

    Add files via upload

---
+++ PB17EXAM/PB17EXAM/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PB17EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //17.Determianti cel mai mare divizor comun si cel mai mic multiplu comun a doua numere. Folositi algoritmul lui Euclid. 
+
+            int a, b;
+
+            a = int.Parse(Console.ReadLine());
+
+            b = int.Parse(Console.ReadLine());
+
+            while (a!=b)
+            {
+                if (a > b) a = a - b;
+                else b = b - a;
+            }
+            Console.WriteLine("CMMDC este " + a);
+        }
+    }
+}
