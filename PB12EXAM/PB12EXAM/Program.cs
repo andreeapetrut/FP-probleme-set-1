48d569e111716287ae58f75cbf93280793070778
andreeapetrut <91960428+andreeapetrut@users.noreply.github.com>
2022-01-14 17:48:48 +0200

    Add files via upload

---
+++ PB12EXAM/PB12EXAM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PB12EXAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //12.Determinati cate numere integi divizibile cu n se afla in intervalul [a, b]. 
+
+            int a, b, n, nr = 0;
+
+            a = int.Parse(Console.ReadLine());
+
+            b = int.Parse(Console.ReadLine());
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i=a;i<=b;i++)
+            {
+                if (i % n == 0) nr++;
+            }
+            Console.WriteLine("Exista " + nr + " numere divizibile cu " + n + " in intervalul [" + a + "," + b + "]");
+        }
+    }
+}
